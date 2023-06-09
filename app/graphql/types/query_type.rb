6ac1cb9032aaf06f068d20068b2ecb2627d0d07f
hiroki141 <ifuku14ll@gmail.com>
2023-06-09 19:39:41 +0900

    remove unnecessary query and mutation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -18,12 +18,5 @@ module Types
     def users
       User.all
     end
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
   end
 end
