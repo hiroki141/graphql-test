49efbd2b3ad02b5db1018fa5728ab032f5047cb7
hiroki141 <ifuku14ll@gmail.com>
2023-06-17 18:08:07 +0900

    rails g graphql:object book

---
+++ app/graphql/types/book_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class BookType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :author, String
+    field :published_on, GraphQL::Types::ISO8601Date
+    field :price, Integer
+    field :user_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
